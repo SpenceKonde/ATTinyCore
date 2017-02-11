6c818c64b54b168451d2ea45d3e23db44a98e11a
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2017-02-11 03:42:55 -0500

    Revert "Revert "Revert "Revert "Revert "Initial commit - this doesn't compile!"""""
    This reverts commit 99dd4ea490ec81e312d0baab7a5ab7d06b7dd776.

--- avr/libraries/SPI/SPI.h
+++ avr/libraries/SPI/SPI.h
@@ -1,42 +1,20 @@
-/*-------------------------------------------------------------------------*
- * tinySPI.h - Arduino hardware SPI master library for ATtiny24/44/84,     *
- * ATtiny25/45/85, and Attiny2313/4313.                                    *
- *                                                                         *
- * Original version of tinyISP by Jack Christensen 24Oct2013               *
- *                                                                         *
- * Added support for Attiny24/25, and Attiny2313/4313                      *
- * by Leonardo Miliani 28Nov2014                                           *               
- *                                                                         *
- * Reworked to add support for 87/167, 261/461/861, and include the normal * 
- * SPI library on chips that have hardware SPI.                            *
- * Spence Konde 2016 (in progress)                                         *
- *                                                                         *
- * CC BY-SA-NC:                                                            *
- * This work is licensed under the Creative Commons Attribution-           *
- * ShareAlike- Not Commercial 4.0 Unported License. To view a copy of this *
- * license, visit                                                          *
- * http://creativecommons.org/licenses/by-sa/4.0/ or send a                *
- * letter to Creative Commons, 171 Second Street, Suite 300,               *
- * San Francisco, California, 94105, USA.                                  *
- *-------------------------------------------------------------------------*/
- 
-#ifndef tinycoreSPI_h
-#define tinycoreSPI_h
+/*
+ * Copyright (c) 2010 by Cristian Maglie <c.maglie@arduino.cc>
+ * Copyright (c) 2014 by Paul Stoffregen <paul@pjrc.com> (Transaction API)
+ * Copyright (c) 2014 by Matthijs Kooijman <matthijs@stdin.nl> (SPISettings AVR)
+ * Copyright (c) 2014 by Andrew J. Kroll <xxxajk@gmail.com> (atomicity fixes)
+ * SPI Master library for arduino.
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of either the GNU General Public License version 2
+ * or the GNU Lesser General Public License version 2.1, both as
+ * published by the Free Software Foundation.
+ */
 
-#include <stdint.h>
-#include <avr/io.h>
-#include <util/atomic.h>
+#ifndef _SPI_H_INCLUDED
+#define _SPI_H_INCLUDED
+
 #include <Arduino.h>
-
-#ifndef LSBFIRST
-#define LSBFIRST 0
-#endif
-#ifndef MSBFIRST
-#define MSBFIRST 1
-#endif
-
-#ifdef SPDR //Then we have hardware SPI, let's use it:
-
 
 // SPI_HAS_TRANSACTION means SPI has beginTransaction(), endTransaction(),
 // usingInterrupt(), and SPISetting(clock, bitOrder, dataMode)
@@ -57,6 +35,13 @@
 // each SPI.beginTransaction().  Connect an LED to this pin.  The LED will turn
 // on if any mismatch is ever detected.
 //#define SPI_TRANSACTION_MISMATCH_LED 5
+
+#ifndef LSBFIRST
+#define LSBFIRST 0
+#endif
+#ifndef MSBFIRST
+#define MSBFIRST 1
+#endif
 
 #define SPI_CLOCK_DIV4 0x00
 #define SPI_CLOCK_DIV16 0x01
@@ -83,6 +68,89 @@
 #elif defined(GIMSK)
   #define SPI_AVR_EIMSK  GIMSK
 #endif
+
+class SPISettings {
+public:
+  SPISettings(uint32_t clock, uint8_t bitOrder, uint8_t dataMode) {
+    if (__builtin_constant_p(clock)) {
+      init_AlwaysInline(clock, bitOrder, dataMode);
+    } else {
+      init_MightInline(clock, bitOrder, dataMode);
+    }
+  }
+  SPISettings() {
+    init_AlwaysInline(4000000, MSBFIRST, SPI_MODE0);
+  }
+private:
+  void init_MightInline(uint32_t clock, uint8_t bitOrder, uint8_t dataMode) {
+    init_AlwaysInline(clock, bitOrder, dataMode);
+  }
+  void init_AlwaysInline(uint32_t clock, uint8_t bitOrder, uint8_t dataMode)
+    __attribute__((__always_inline__)) {
+    // Clock settings are defined as follows. Note that this shows SPI2X
+    // inverted, so the bits form increasing numbers. Also note that
+    // fosc/64 appears twice
+    // SPR1 SPR0 ~SPI2X Freq
+    //   0    0     0   fosc/2
+    //   0    0     1   fosc/4
+    //   0    1     0   fosc/8
+    //   0    1     1   fosc/16
+    //   1    0     0   fosc/32
+    //   1    0     1   fosc/64
+    //   1    1     0   fosc/64
+    //   1    1     1   fosc/128
+
+    // We find the fastest clock that is less than or equal to the
+    // given clock rate. The clock divider that results in clock_setting
+    // is 2 ^^ (clock_div + 1). If nothing is slow enough, we'll use the
+    // slowest (128 == 2 ^^ 7, so clock_div = 6).
+    uint8_t clockDiv;
+
+    // When the clock is known at compiletime, use this if-then-else
+    // cascade, which the compiler knows how to completely optimize
+    // away. When clock is not known, use a loop instead, which generates
+    // shorter code.
+    if (__builtin_constant_p(clock)) {
+      if (clock >= F_CPU / 2) {
+        clockDiv = 0;
+      } else if (clock >= F_CPU / 4) {
+        clockDiv = 1;
+      } else if (clock >= F_CPU / 8) {
+        clockDiv = 2;
+      } else if (clock >= F_CPU / 16) {
+        clockDiv = 3;
+      } else if (clock >= F_CPU / 32) {
+        clockDiv = 4;
+      } else if (clock >= F_CPU / 64) {
+        clockDiv = 5;
+      } else {
+        clockDiv = 6;
+      }
+    } else {
+      uint32_t clockSetting = F_CPU / 2;
+      clockDiv = 0;
+      while (clockDiv < 6 && clock < clockSetting) {
+        clockSetting /= 2;
+        clockDiv++;
+      }
+    }
+
+    // Compensate for the duplicate fosc/64
+    if (clockDiv == 6)
+    clockDiv = 7;
+
+    // Invert the SPI2X bit
+    clockDiv ^= 0x1;
+
+    // Pack into the SPISettings class
+    spcr = _BV(SPE) | _BV(MSTR) | ((bitOrder == LSBFIRST) ? _BV(DORD) : 0) |
+      (dataMode & SPI_MODE_MASK) | ((clockDiv >> 1) & SPI_CLOCK_MASK);
+    spsr = clockDiv & SPI_2XCLOCK_MASK;
+  }
+  uint8_t spcr;
+  uint8_t spsr;
+  friend class SPIClass;
+};
 
 
 class SPIClass {
@@ -139,12 +207,12 @@ public:
   // Write to the SPI bus (MOSI pin) and also receive (MISO pin)
   inline static uint8_t transfer(uint8_t data) {
     SPDR = data;
-    //
-    // The following NOP introduces a small delay that can prevent the wait
-   /// loop form iterating when running at the maximum speed. This gives
-    // about 10% more speed, even if it seems counter-intuitive. At lower
-    // speeds it is unnoticed.
-    ///
+    /*
+     * The following NOP introduces a small delay that can prevent the wait
+     * loop form iterating when running at the maximum speed. This gives
+     * about 10% more speed, even if it seems counter-intuitive. At lower
+     * speeds it is unnoticed.
+     */
     asm volatile("nop");
     while (!(SPSR & _BV(SPIF))) ; // wait
     return SPDR;
@@ -250,163 +318,7 @@ private:
   static uint8_t inTransactionFlag;
   #endif
 };
+
 extern SPIClass SPI;
-class SPISettings {
-public:
-  SPISettings(uint32_t clock, uint8_t bitOrder, uint8_t dataMode) {
-    if (__builtin_constant_p(clock)) {
-      init_AlwaysInline(clock, bitOrder, dataMode);
-    } else {
-      init_MightInline(clock, bitOrder, dataMode);
-    }
-  }
-  SPISettings() {
-    init_AlwaysInline(4000000, MSBFIRST, SPI_MODE0);
-  }
-private:
-  void init_MightInline(uint32_t clock, uint8_t bitOrder, uint8_t dataMode) {
-    init_AlwaysInline(clock, bitOrder, dataMode);
-  }
-  void init_AlwaysInline(uint32_t clock, uint8_t bitOrder, uint8_t dataMode)
-    __attribute__((__always_inline__)) {
-    // Clock settings are defined as follows. Note that this shows SPI2X
-    // inverted, so the bits form increasing numbers. Also note that
-    // fosc/64 appears twice
-    // SPR1 SPR0 ~SPI2X Freq
-    //   0    0     0   fosc/2
-    //   0    0     1   fosc/4
-    //   0    1     0   fosc/8
-    //   0    1     1   fosc/16
-    //   1    0     0   fosc/32
-    //   1    0     1   fosc/64
-    //   1    1     0   fosc/64
-    //   1    1     1   fosc/128
 
-    // We find the fastest clock that is less than or equal to the
-    // given clock rate. The clock divider that results in clock_setting
-    // is 2 ^^ (clock_div + 1). If nothing is slow enough, we'll use the
-    // slowest (128 == 2 ^^ 7, so clock_div = 6).
-    uint8_t clockDiv;
-
-    // When the clock is known at compiletime, use this if-then-else
-    // cascade, which the compiler knows how to completely optimize
-    // away. When clock is not known, use a loop instead, which generates
-    // shorter code.
-    if (__builtin_constant_p(clock)) {
-      if (clock >= F_CPU / 2) {
-        clockDiv = 0;
-      } else if (clock >= F_CPU / 4) {
-        clockDiv = 1;
-      } else if (clock >= F_CPU / 8) {
-        clockDiv = 2;
-      } else if (clock >= F_CPU / 16) {
-        clockDiv = 3;
-      } else if (clock >= F_CPU / 32) {
-        clockDiv = 4;
-      } else if (clock >= F_CPU / 64) {
-        clockDiv = 5;
-      } else {
-        clockDiv = 6;
-      }
-    } else {
-      uint32_t clockSetting = F_CPU / 2;
-      clockDiv = 0;
-      while (clockDiv < 6 && clock < clockSetting) {
-        clockSetting /= 2;
-        clockDiv++;
-      }
-    }
-
-    // Compensate for the duplicate fosc/64
-    if (clockDiv == 6)
-    clockDiv = 7;
-
-    // Invert the SPI2X bit
-    clockDiv ^= 0x1;
-
-    // Pack into the SPISettings class
-    spcr = _BV(SPE) | _BV(MSTR) | ((bitOrder == LSBFIRST) ? _BV(DORD) : 0) |
-      (dataMode & SPI_MODE_MASK) | ((clockDiv >> 1) & SPI_CLOCK_MASK);
-    spsr = clockDiv & SPI_2XCLOCK_MASK;
-  }
-  uint8_t spcr;
-  uint8_t spsr;
-  friend class SPIClass;
-};
-
-#else 
-#ifdef USICR //if we have a USI instead, use that
-#define SPI_MODE0 0x00
-#define SPI_MODE1 0x04
-class SPISettings {
-public:
-  SPISettings(uint32_t clock, uint8_t bitOrder, uint8_t dataMode) {
-    init_AlwaysInline(clock, bitOrder, dataMode);
-  }
-  SPISettings() {
-    init_AlwaysInline(4000000, MSBFIRST, SPI_MODE0);
-  }
-private:
-  void init_AlwaysInline(uint32_t clock, uint8_t bitOrder, uint8_t dataMode)
-    __attribute__((__always_inline__)) {
-    usicr=(bitOrder==SPI_MODE1)?0x1E:0x1A;
-  }
-  uint8_t usicr;
-  friend class tinySPI;
-};
-
-//SPI data modes
-#define SPI_MODE0 0x00
-#define SPI_MODE1 0x04
-
-class tinySPI
-{
- public:
-  tinySPI();
-  static uint8_t reverse(uint8_t x);
-  static void begin();
-  inline static void beginTransaction(SPISettings settings);
-  inline static uint8_t transfer(uint8_t data);
-  inline static uint16_t transfer16(uint16_t data);
-  inline static void transfer(void *buf, size_t count);
-  inline static void endTransaction(void);
-  static void end();
-
-  // This function is deprecated.  New applications should use
-  // beginTransaction() to configure SPI settings.
-  inline static void setBitOrder(uint8_t bitOrder) {reversebit=bitOrder;}
-  // This function is deprecated.  New applications should use
-  // beginTransaction() to configure SPI settings.
-  inline static void setDataMode(uint8_t dataMode);
-  // This function is deprecated.  New applications should use
-  // beginTransaction() to configure SPI settings.
-  inline static void setClockDivider(uint8_t clockDiv);
-  // These undocumented functions should not be used.  SPI.transfer()
-  // polls the hardware flag which is automatically cleared as the
-  // AVR responds to SPI's interrupt
-  //inline static void attachInterrupt() { SPCR |= _BV(SPIE); }
-  //inline static void detachInterrupt() { SPCR &= ~_BV(SPIE); }
-
-private:
-  static uint8_t initialized;
-  static uint8_t reversebit;
-  //static uint8_t interruptMode; // 0=none, 1=mask, 2=global
-  //static uint8_t interruptMask; // which interrupts to mask
-  //static uint8_t interruptSave; // temp storage, to restore state
-};
-
-extern tinySPI SPI;
-
-
-
-#else 
-//if no USICR and no TWBR
-
-#error No supported hardware
-
-#endif //end if USICR
-#endif //end if TWBR
-#endif //end of module include guard
-
-
-
+#endif
