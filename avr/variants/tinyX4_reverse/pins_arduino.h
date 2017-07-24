ddcc5652904d8c011bb3923fbb3a03228a1b8fcc
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2017-07-24 12:31:37 -0400

    Update pins_arduino.h

--- avr/variants/tinyX4_reverse/pins_arduino.h
+++ avr/variants/tinyX4_reverse/pins_arduino.h
@@ -73,14 +73,14 @@ static const uint8_t SCL = 4;
 
 //Ax constants should not be used for digitalRead/digitalWrite/analogWrite functions, only analogRead().
 //#define ANALOG_PINS_ARE_ADC_NUMBERS 1
-static const uint8_t A0 = 0x80 & 0;
-static const uint8_t A1 = 0x80 & 1;
-static const uint8_t A2 = 0x80 & 2;
-static const uint8_t A3 = 0x80 & 3;
-static const uint8_t A4 = 0x80 & 4;
-static const uint8_t A5 = 0x80 & 5;
-static const uint8_t A6 = 0x80 & 6;
-static const uint8_t A7 = 0x80 & 7;
+static const uint8_t A0 = 0x80 | 0;
+static const uint8_t A1 = 0x80 | 1;
+static const uint8_t A2 = 0x80 | 2;
+static const uint8_t A3 = 0x80 | 3;
+static const uint8_t A4 = 0x80 | 4;
+static const uint8_t A5 = 0x80 | 5;
+static const uint8_t A6 = 0x80 | 6;
+static const uint8_t A7 = 0x80 | 7;
 #define PIN_A0  (0)
 #define PIN_A1  ( 1)
 #define PIN_A2  ( 2)
