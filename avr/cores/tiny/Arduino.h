05e08484b4ccfedeadceff8119d1ec17753e7ae4
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2018-02-23 13:15:05 -0500

    Fix #193
    Looks like those defines snuck in from the old days, as they should not be needed and are not present on official core.

--- avr/cores/tiny/Arduino.h
+++ avr/cores/tiny/Arduino.h
@@ -26,8 +26,6 @@ void yield(void);
 #define OUTPUT 0x1
 #define INPUT_PULLUP 0x2
 
-#define true 0x1
-#define false 0x0
 
 #define PI 3.1415926535897932384626433832795
 #define HALF_PI 1.5707963267948966192313216916398
