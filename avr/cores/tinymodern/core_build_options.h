064860b19a8ade773ed1ed3d6257a51ce24cb631
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2018-05-03 11:55:17 -0400

    Move tone() to timer2 on attinyx41.

--- avr/cores/tinymodern/core_build_options.h
+++ avr/cores/tinymodern/core_build_options.h
@@ -48,13 +48,9 @@
 #define TIMER_TO_USE_FOR_MILLIS                   0
 
 /*
-  Tone goes on whichever timer was not used for millis.
+  Tone goes on timer2 for maximum compatibility
 */
-#if TIMER_TO_USE_FOR_MILLIS == 1
-#define TIMER_TO_USE_FOR_TONE                     0
-#else
-#define TIMER_TO_USE_FOR_TONE                     1
-#endif
+#define TIMER_TO_USE_FOR_TONE                     2
 
 #define HAVE_ADC                                  1
 
