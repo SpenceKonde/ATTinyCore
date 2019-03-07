d5716a05ef3caa94d0c73de0261316272773f3d9
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2019-03-07 16:20:06 -0500

    Update wiring.c
    Fix missing semicolons, and pull in fix from not-modern core.

--- avr/cores/tinymodern/wiring.c
+++ avr/cores/tinymodern/wiring.c
@@ -122,6 +122,19 @@ unsigned long millis()
 
   return m;
 }
+unsigned long millis()
+{
+  unsigned long m;
+  uint8_t oldSREG = SREG;
+
+  // disable interrupts while we read millis_timer_millis or we might get an
+  // inconsistent value (e.g. in the middle of a write to millis_timer_millis)
+  cli();
+  m = millis_timer_millis;
+  SREG = oldSREG;
+
+  return m;
+}
 
 unsigned long micros() 
 {
@@ -130,46 +143,70 @@ unsigned long micros()
   
   cli();
   m = millis_timer_overflow_count;
-  t = MillisTimer_GetCount();
+#if defined(TCNT0) && (TIMER_TO_USE_FOR_MILLIS == 0) && !defined(TCW0)
+  t = TCNT0;
+#elif defined(TCNT0L) && (TIMER_TO_USE_FOR_MILLIS == 0)
+  t = TCNT0L;
+#elif defined(TCNT1) && (TIMER_TO_USE_FOR_MILLIS == 1)
+  t = TCNT1;
+#elif defined(TCNT1L) && (TIMER_TO_USE_FOR_MILLIS == 1)
+  t = TCNT1L;
+#else
+  #error Millis()/Micros() timer not defined
+#endif
   
-  if (MillisTimer_IsOverflowSet() && (t < 255))
+#if defined(TIFR0) && (TIMER_TO_USE_FOR_MILLIS == 0)
+  if ((TIFR0 & _BV(TOV0)) && (t < 255))
     m++;
-  
+#elif defined(TIFR) && (TIMER_TO_USE_FOR_MILLIS == 0)
+  if ((TIFR & _BV(TOV0)) && (t < 255))
+    m++;
+#elif defined(TIFR1) && (TIMER_TO_USE_FOR_MILLIS == 1)
+  if ((TIFR1 & _BV(TOV1)) && (t < 255))
+    m++;
+#elif defined(TIFR) && (TIMER_TO_USE_FOR_MILLIS == 1)
+  if ((TIFR & _BV(TOV1)) && (t < 255))
+    m++;
+#endif
 
   SREG = oldSREG;
-#if (MillisTimer_Prescale_Value % clockCyclesPerMicrosecond() == 0 ) //Can we just do it the naive way? If so great!
+
+
+#if F_CPU < 1000000L
+  return ((m << 8) + t) * MillisTimer_Prescale_Value * (1000000L/F_CPU);
+#else
+#if (MillisTimer_Prescale_Value % clockCyclesPerMicrosecond() == 0 ) // Can we just do it the naive way? If so great!
   return ((m << 8) + t) * (MillisTimer_Prescale_Value / clockCyclesPerMicrosecond());
-//Otherwise we have a problem.
-#elif (MillisTimer_Prescale_Value == 64 && clockCyclesPerMicrosecond() == 11) // 5.75 vs real value 5.818 (11mhz) 5.78 (11.059)
-  m=(m << 8) + t;
-  return m+(m<<2)+(m>>1)+(m>>2);
-#elif (MillisTimer_Prescale_Value == 64 && clockCyclesPerMicrosecond() == 12) // 5.3125 vs real value 5.333
-  m=(m << 8) + t;
-  return m+(m<<2)+(m>>2)+(m>>4);
+  // Otherwise we do clock-specific calculations
+#elif (MillisTimer_Prescale_Value == 64 && F_CPU == 24000000L) // 2.6875 vs real value 2.67 
+  m = (m << 8) + t;
+  return (m<<1) + (m >> 1) + (m >> 3) + (m >> 4); // multiply by 2.6875
 #elif (MillisTimer_Prescale_Value == 64 && clockCyclesPerMicrosecond() == 20) // 3.187 vs real value 3.2
   m=(m << 8) + t;
   return m+(m<<1)+(m>>2)-(m>>4);
 #elif (MillisTimer_Prescale_Value == 64 && F_CPU == 18432000L) // 3.5 vs real value 3.47
   m=(m << 8) + t;
-  return m+(m<<1)+(m>>1)
+  return m+(m<<1)+(m>>1);
 #elif (MillisTimer_Prescale_Value == 64 && F_CPU==14745600L) //4.375  vs real value 4.34
   m=(m << 8) + t;
   return (m<<2)+(m>>1)-(m>>3);
 #elif (MillisTimer_Prescale_Value == 64 && clockCyclesPerMicrosecond() == 14) //4.5 - actual 4.57 for 14.0mhz, 4.47 for the 14.3 crystals scrappable from everything
   m=(m << 8) + t;
-  return (m<<2)+(m>>1)
-#elif (MillisTimer_Prescale_Value == 64 && F_CPU==7372800L) //8.625, vs real value 8.68
+  return (m<<2)+(m>>1);
+#elif (MillisTimer_Prescale_Value == 64 && clockCyclesPerMicrosecond() == 12) // 5.3125 vs real value 5.333
+  m=(m << 8) + t;
+  return m+(m<<2)+(m>>2)+(m>>4);
+#elif (MillisTimer_Prescale_Value == 64 && clockCyclesPerMicrosecond() == 11) // 5.75 vs real value 5.818 (11mhz) 5.78 (11.059)
+  m=(m << 8) + t;
+  return m+(m<<2)+(m>>1)+(m>>2);
+#elif (MillisTimer_Prescale_Value == 64 && F_CPU==7372800L) // 8.625, vs real value 8.68
   m=(m << 8) + t;
   return (m<<3)+(m>>2)+(m>>3);
-#elif (MillisTimer_Prescale_Value == 64 && F_CPU==6000000L) //10.625, vs real value 10.67
+#elif (MillisTimer_Prescale_Value == 64 && F_CPU==6000000L) // 10.625, vs real value 10.67
   m=(m << 8) + t;
   return (m<<3)+(m<<1)+(m>>2)+(m>>3);
-#elif (MillisTimer_Prescale_Value == 64 && clockCyclesPerMicrosecond() == 9) //for 9mhz, this is a little off, but for 9.21, it's very close!
+#elif (MillisTimer_Prescale_Value == 64 && clockCyclesPerMicrosecond() == 9) // For 9mhz, this is a little off, but for 9.21, it's very close!
   return ((m << 8) + t) * (MillisTimer_Prescale_Value / clockCyclesPerMicrosecond());
-#elif (MillisTimer_Prescale_Value == 64 && F_CPU==8200000L)
-  m = (m<<8)+t;
-  return (m<<2)+(m<<1)+m-(m>>2); //7.75 
-  
 #else
   //return ((m << 8) + t) * (MillisTimer_Prescale_Value / clockCyclesPerMicrosecond());
   //return ((m << 8) + t) * MillisTimer_Prescale_Value / clockCyclesPerMicrosecond();
@@ -180,11 +217,7 @@ unsigned long micros()
   //the high part gets divided by cCPuS then multiplied by the prescaler. Then take the low 8 bits plus the high part modulo-cCPuS to correct for the division, then multiply that by the prescaler value first before dividing by cCPuS, and finally add the two together.
   //return ((m << 8 )/clockCyclesPerMicrosecond()* MillisTimer_Prescale_Value) + ((t+(((m<<8)%clockCyclesPerMicrosecond())) * MillisTimer_Prescale_Value / clockCyclesPerMicrosecond()));
   return ((m << 8 )/clockCyclesPerMicrosecond()* MillisTimer_Prescale_Value) + (t * MillisTimer_Prescale_Value / clockCyclesPerMicrosecond());
-  
-  //This doesn't work, and I don't know why:
-  //return ((m*(unsigned long)MillisTimer_Prescale_Value / (unsigned long)clockCyclesPerMicrosecond())<<8)+(((unsigned long)t+((m%11)<<8)) * (unsigned long)MillisTimer_Prescale_Value / (unsigned long)clockCyclesPerMicrosecond());
-  //This works without the loss of precision, but eats an extra 380 bytes of flash
-  //return (((long long)((m << 8) + t)) * MillisTimer_Prescale_Value / clockCyclesPerMicrosecond()); //very disappointing fix, eats an extra 380 bytes of flash because of long long
+#endif
 #endif
 }
 
