7d965284ee65de834b36e971475de8b02c6cc1a3
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2019-05-23 02:26:10 -0400

    update makeall.tiny.bat, fix makefile
    somehow when I removed the trailing whitespace, I also reverted the
    changes...

--- avr/bootloaders/optiboot/makeall.tiny.bat
+++ avr/bootloaders/optiboot/makeall.tiny.bat
@@ -8,6 +8,8 @@ call omake attiny841at16
 call omake attiny841at12
 call omake attiny841at8
 call omake attiny841at8_5v
+call omake attiny841at1
+call omake attiny841at1_5v
 
 call omake attiny841at184ser1
 call omake attiny841at147ser1
@@ -18,6 +20,7 @@ call omake attiny841at20ser1
 call omake attiny841at16ser1
 call omake attiny841at12ser1
 call omake attiny841at8ser1
+call omake attiny841at1ser1
 
 call omake attiny441at184
 call omake attiny441at147
@@ -29,6 +32,8 @@ call omake attiny441at16
 call omake attiny441at12
 call omake attiny441at8
 call omake attiny441at8_5v
+call omake attiny441at1
+call omake attiny441at1_5v
 
 call omake attiny441at184ser1
 call omake attiny441at147ser1
@@ -39,6 +44,7 @@ call omake attiny441at20ser1
 call omake attiny441at16ser1
 call omake attiny441at12ser1
 call omake attiny441at8ser1
+call omake attiny441at1ser1
 
 call omake attiny87at184
 call omake attiny87at147
@@ -49,6 +55,7 @@ call omake attiny87at20
 call omake attiny87at16
 call omake attiny87at12
 call omake attiny87at8
+call omake attiny87at1
 
 call omake attiny167at184
 call omake attiny167at147
@@ -59,6 +66,7 @@ call omake attiny167at20
 call omake attiny167at16
 call omake attiny167at12
 call omake attiny167at8
+call omake attiny167at1
 
 call omake attiny1634at147
 call omake attiny1634at110
@@ -67,8 +75,10 @@ call omake attiny1634at737
 call omake attiny1634at20
 call omake attiny1634at16
 call omake attiny1634at12
-call omake attiny1634at8
-call omake attiny1634at8_5v
+call omake attiny1634at1
+call omake attiny1634at1_5v
+call omake attiny1634at1
+call omake attiny1634at1_5v
 
 call omake attiny1634at147ser1
 call omake attiny1634at110ser1
@@ -78,10 +88,13 @@ call omake attiny1634at20ser1
 call omake attiny1634at16ser1
 call omake attiny1634at12ser1
 call omake attiny1634at8ser1
+call omake attiny1634at8ser1
 
 
 call omake attiny828at8
 call omake attiny828at8_5v
+call omake attiny828at1
+call omake attiny828at1_5v
 
 call omake attiny85at184
 call omake attiny85at147
@@ -92,6 +105,7 @@ call omake attiny85at20
 call omake attiny85at16
 call omake attiny85at12
 call omake attiny85at8
+call omake attiny85at1
 
 call omake attiny45at184
 call omake attiny45at147
@@ -102,6 +116,7 @@ call omake attiny45at20
 call omake attiny45at16
 call omake attiny45at12
 call omake attiny45at8
+call omake attiny45at1
 
 call omake attiny84at184
 call omake attiny84at147
@@ -112,6 +127,7 @@ call omake attiny84at20
 call omake attiny84at16
 call omake attiny84at12
 call omake attiny84at8
+call omake attiny84at1
 
 call omake attiny44at184
 call omake attiny44at147
@@ -122,6 +138,7 @@ call omake attiny44at20
 call omake attiny44at16
 call omake attiny44at12
 call omake attiny44at8
+call omake attiny44at1
 
 call omake attiny861at184
 call omake attiny861at147
@@ -132,6 +149,7 @@ call omake attiny861at20
 call omake attiny861at16
 call omake attiny861at12
 call omake attiny861at8
+call omake attiny861at1
 
 call omake attiny461at184
 call omake attiny461at147
@@ -142,3 +160,4 @@ call omake attiny461at20
 call omake attiny461at16
 call omake attiny461at12
 call omake attiny461at8
+call omake attiny461at1
