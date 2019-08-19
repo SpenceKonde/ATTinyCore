5927a47b187129f780b4f286da65d2fb2e181742
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2019-08-18 21:38:34 -0400

    Support 14.7456MHz for neopixel
    And other speeds around there.

--- avr/libraries/tinyNeoPixel_Static/tinyNeoPixel_Static.cpp
+++ avr/libraries/tinyNeoPixel_Static/tinyNeoPixel_Static.cpp
@@ -480,6 +480,77 @@ void tinyNeoPixel::show(void) {
           [hi]     "r" (hi),
           [lo]     "r" (lo));
 
+// 14.7 MHz(ish) AVR --------------------------------------------------------
+#elif (F_CPU >= 14300000UL) && (F_CPU <= 15400000UL)
+
+    // In the 14.7 MHz case, an optimized 800 KHz datastream (no dead time
+    // between bytes) requires a PORT-specific loop similar to the 8 MHz
+    // code (but a little more relaxed in this case).
+
+    // 18 instruction clocks per bit: HHHHHxxxxxxxLLLLLL
+    // OUT instructions:              ^    ^      ^     (T=0,5,12)
+
+    volatile uint8_t next;
+
+      hi   = NEOPIXELPORT |  pinMask;
+      lo   = NEOPIXELPORT & ~pinMask;
+      next = lo;
+      if(b & 0x80) next = hi;
+
+      // Don't "optimize" the OUT calls into the bitTime subroutine;
+      // we're exploiting the RCALL and RET as 3- and 4-cycle NOPs!
+      asm volatile(
+       "headD:"                   "\n\t" //        (T =  0)
+        "out   %[port], %[hi]"    "\n\t" //        (T =  1)
+        "rcall bitTimeD"          "\n\t" // Bit 7  (T = 17)
+        "out   %[port], %[hi]"    "\n\t"
+        "rcall bitTimeD"          "\n\t" // Bit 6
+        "out   %[port], %[hi]"    "\n\t"
+        "rcall bitTimeD"          "\n\t" // Bit 5
+        "out   %[port], %[hi]"    "\n\t"
+        "rcall bitTimeD"          "\n\t" // Bit 4
+        "out   %[port], %[hi]"    "\n\t"
+        "rcall bitTimeD"          "\n\t" // Bit 3
+        "out   %[port], %[hi]"    "\n\t"
+        "rcall bitTimeD"          "\n\t" // Bit 2
+        "out   %[port], %[hi]"    "\n\t"
+        "rcall bitTimeD"          "\n\t" // Bit 1
+        // Bit 0:
+        "out  %[port] , %[hi]"    "\n\t" // 1    PORT = hi    (T =  1)
+        "rjmp .+0"                "\n\t" // 2    nop nop      (T =  3)
+        "ld   %[byte] , %a[ptr]+" "\n\t" // 2    b = *ptr++   (T =  5)
+        "out  %[port] , %[next]"  "\n\t" // 1    PORT = next  (T =  6)
+        "mov  %[next] , %[lo]"    "\n\t" // 1    next = lo    (T =  7)
+        "sbrc %[byte] , 7"        "\n\t" // 1-2  if(b & 0x80) (T =  8)
+         "mov %[next] , %[hi]"    "\n\t" // 0-1    next = hi  (T =  9)
+        "rjmp .+0"                "\n\t" // 2    nop nop      (T = 11)
+        "nop"                     "\n\t" // 1                 (T = 12)
+        "out  %[port] , %[lo]"    "\n\t" // 1    PORT = lo    (T = 13)
+        "nop"                     "\n\t" // 1                 (T = 14)
+        "sbiw %[count], 1"        "\n\t" // 2    i--          (T = 16)
+        "brne headD"              "\n\t" // 2    if(i != 0) -> (next byte)
+         "rjmp doneD"             "\n\t"
+        "bitTimeD:"               "\n\t" //      nop nop nop     (T =  4)
+         "nop"                    "\n\t" // 1                    (T =  5)
+         "out  %[port], %[next]"  "\n\t" // 1    PORT = next     (T =  6)
+         "mov  %[next], %[lo]"    "\n\t" // 1    next = lo       (T =  7)
+         "rol  %[byte]"           "\n\t" // 1    b <<= 1         (T =  8)
+         "sbrc %[byte], 7"        "\n\t" // 1-2  if(b & 0x80)    (T =  9)
+          "mov %[next], %[hi]"    "\n\t" // 0-1   next = hi      (T = 10)
+         "rjmp .+0"               "\n\t" // 2    nop nop         (T = 12)
+         "out  %[port], %[lo]"    "\n\t" // 1    PORT = lo       (T = 13)
+         "nop"                    "\n\t" // 1                    (T = 14)
+         "ret"                    "\n\t" // 4    nop nop nop nop (T = 18)
+         "doneD:"                 "\n"
+        : [byte]  "+r" (b),
+          [next]  "+r" (next),
+          [count] "+w" (i)
+        : [port]   "I" (_SFR_IO_ADDR(NEOPIXELPORT)),
+          [ptr]    "e" (ptr),
+          [hi]     "r" (hi),
+          [lo]     "r" (lo));
+
+
 
 // 16 MHz(ish) AVR --------------------------------------------------------
 #elif (F_CPU >= 15400000UL) && (F_CPU <= 19000000L)
