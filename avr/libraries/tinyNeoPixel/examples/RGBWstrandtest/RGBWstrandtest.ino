21978af668dfd36ff94ad100a5e1a2b79f92950d
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2019-08-23 13:09:33 -0400

    Why are we using an int when a byte would work?
    This is mainly an issue for travis testing, as this bloats the ram use to the point where it won't compile on most parts.

--- avr/libraries/tinyNeoPixel/examples/RGBWstrandtest/RGBWstrandtest.ino
+++ avr/libraries/tinyNeoPixel/examples/RGBWstrandtest/RGBWstrandtest.ino
@@ -3,13 +3,13 @@
 
 #define PIN 6
 
-#define NUM_LEDS 60
+#define NUM_LEDS 20
 
 #define BRIGHTNESS 50
 
 tinyNeoPixel strip = tinyNeoPixel(NUM_LEDS, PIN, NEO_GRBW + NEO_KHZ800);
 
-int gamma[] = {
+byte gamma[] = {
     0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
     0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
     1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
