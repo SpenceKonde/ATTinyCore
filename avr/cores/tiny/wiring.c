512fd8e75309845a99ae1aaf39a247dce361473b
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2019-10-04 21:08:41 -0400

    trailing whitespace

--- avr/cores/tiny/wiring.c
+++ avr/cores/tiny/wiring.c
@@ -836,7 +836,7 @@ void init(void)
   #endif
   // this needs to be called before setup() or some functions won't work there
   sei();
-  
+
   #ifndef DISABLEMILLIS
   // Enable the overlow interrupt (this is the basic system tic-toc for millis)
   #if defined(TIMSK) && defined(TOIE0) && (TIMER_TO_USE_FOR_MILLIS == 0)
