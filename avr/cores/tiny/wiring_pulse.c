25c862bd89c72e3026583ebc253a3f3f348942e9
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2020-02-05 14:42:11 -0500

    convert tabs to spaces
    was causing travis failure

--- avr/cores/tiny/wiring_pulse.c
+++ avr/cores/tiny/wiring_pulse.c
@@ -32,24 +32,24 @@
  */
 unsigned long pulseIn(uint8_t pin, uint8_t state, unsigned long timeout)
 {
-	// cache the port and bit of the pin in order to speed up the
-	// pulse width measuring loop and achieve finer resolution.  calling
-	// digitalRead() instead yields much coarser resolution.
-	uint8_t bit = digitalPinToBitMask(pin);
-	uint8_t port = digitalPinToPort(pin);
-	uint8_t stateMask = (state ? bit : 0);
+    // cache the port and bit of the pin in order to speed up the
+    // pulse width measuring loop and achieve finer resolution.  calling
+    // digitalRead() instead yields much coarser resolution.
+    uint8_t bit = digitalPinToBitMask(pin);
+    uint8_t port = digitalPinToPort(pin);
+    uint8_t stateMask = (state ? bit : 0);
 
-	// convert the timeout from microseconds to a number of times through
-	// the initial loop; it takes approximately 16 clock cycles per iteration
-	unsigned long maxloops = microsecondsToClockCycles(timeout)/16;
+    // convert the timeout from microseconds to a number of times through
+    // the initial loop; it takes approximately 16 clock cycles per iteration
+    unsigned long maxloops = microsecondsToClockCycles(timeout)/16;
 
-	unsigned long width = countPulseASM(portInputRegister(port), bit, stateMask, maxloops);
+    unsigned long width = countPulseASM(portInputRegister(port), bit, stateMask, maxloops);
 
-	// prevent clockCyclesToMicroseconds to return bogus values if countPulseASM timed out
-	if (width)
-		return clockCyclesToMicroseconds(width * 16 + 16);
-	else
-		return 0;
+    // prevent clockCyclesToMicroseconds to return bogus values if countPulseASM timed out
+    if (width)
+        return clockCyclesToMicroseconds(width * 16 + 16);
+    else
+        return 0;
 }
 
 /* Measures the length (in microseconds) of a pulse on the pin; state is HIGH
@@ -62,32 +62,32 @@ unsigned long pulseIn(uint8_t pin, uint8_t state, unsigned long timeout)
  */
 unsigned long pulseInLong(uint8_t pin, uint8_t state, unsigned long timeout)
 {
-	// cache the port and bit of the pin in order to speed up the
-	// pulse width measuring loop and achieve finer resolution.  calling
-	// digitalRead() instead yields much coarser resolution.
-	uint8_t bit = digitalPinToBitMask(pin);
-	uint8_t port = digitalPinToPort(pin);
-	uint8_t stateMask = (state ? bit : 0);
+    // cache the port and bit of the pin in order to speed up the
+    // pulse width measuring loop and achieve finer resolution.  calling
+    // digitalRead() instead yields much coarser resolution.
+    uint8_t bit = digitalPinToBitMask(pin);
+    uint8_t port = digitalPinToPort(pin);
+    uint8_t stateMask = (state ? bit : 0);
 
-	unsigned long startMicros = micros();
+    unsigned long startMicros = micros();
 
-	// wait for any previous pulse to end
-	while ((*portInputRegister(port) & bit) == stateMask) {
-		if (micros() - startMicros > timeout)
-			return 0;
-	}
+    // wait for any previous pulse to end
+    while ((*portInputRegister(port) & bit) == stateMask) {
+        if (micros() - startMicros > timeout)
+            return 0;
+    }
 
-	// wait for the pulse to start
-	while ((*portInputRegister(port) & bit) != stateMask) {
-		if (micros() - startMicros > timeout)
-			return 0;
-	}
+    // wait for the pulse to start
+    while ((*portInputRegister(port) & bit) != stateMask) {
+        if (micros() - startMicros > timeout)
+            return 0;
+    }
 
-	unsigned long start = micros();
-	// wait for the pulse to stop
-	while ((*portInputRegister(port) & bit) == stateMask) {
-		if (micros() - startMicros > timeout)
-			return 0;
-	}
-	return micros() - start;
+    unsigned long start = micros();
+    // wait for the pulse to stop
+    while ((*portInputRegister(port) & bit) == stateMask) {
+        if (micros() - startMicros > timeout)
+            return 0;
+    }
+    return micros() - start;
 }
