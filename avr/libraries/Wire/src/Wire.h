04369e58695c8ff45fd30b40bdc8cc7c3141feb3
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2020-04-08 07:08:21 -0400

    whitespace broke travis builds...

--- avr/libraries/Wire/src/Wire.h
+++ avr/libraries/Wire/src/Wire.h
@@ -1,7 +1,6 @@
 /*
   TwoWire.h - TWI/I2C library for Arduino & Wiring
   Copyright (c) 2006 Nicholas Zambetti.  All right reserved.
-  
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
