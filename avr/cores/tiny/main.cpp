d9037a9ac26eccc550be121b78174b32f61cde08
Spence Konde <spencekonde@gmail.com>
2021-06-14 21:32:05 -0400

    Fix #475
    there is some bug relating to how the stack gets initialized, which something like this nonsense fixes

--- avr/cores/tiny/main.cpp
+++ avr/cores/tiny/main.cpp
@@ -1,7 +1,16 @@
 #include <Arduino.h>
-
+void __PreventIssue475() {  /* This it TOTAL VOODOO */
+  volatile byte voodoo=0;   /* this line prevents a weird bug from manifesting */
+  GPIOR2=voodoo;            /* and this prevents warnings. In cases were it fixes the bug
+                             * it generates 5-6 instructions which do something with the
+                             * stack pointer. I think under some condition, the stack pointer isn't
+                             * set up properly, even though code elsewhere assumes that it is?
+                             * It's gotta be *something* like that, based on the instructions that
+                             * are present when the bug is fixed */
+}
 int main(void)
 {
+  __PreventIssue475();      /* Anyone care to enlighten me on how this fixes it?! */
   init();
 
   setup();
