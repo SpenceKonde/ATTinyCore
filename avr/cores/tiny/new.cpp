e7c0fd26e78939943d930e83cb3ac342298d6bd7
Spence Konde <spencekonde@gmail.com>
2022-01-27 15:32:38 -0500

    Fix missing delete issues introduced by C++ 17

--- avr/cores/tiny/new.cpp
+++ avr/cores/tiny/new.cpp
@@ -14,11 +14,22 @@
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+  Updated 2020-21 Spence Konde to support C++ 14 sized
+  dealocations, and give useful compile errors when aligned
+  new and delete operators are referenced. I do not think adding
+  support for them is warranted at this time, though we have
+  candidates for the simplest of them, both were written by
+  @henrygab (Henry Gabryjelski, which is fitting because that's
+  who asked for C++ 17 mode, over on ATTinyCore, but releases
+  are more frequent for the new parts, and this is equally
+  applicable to all of the cores.)
 */
 
 #include <stdlib.h>
 
-void *operator new(size_t size) {
+// For C++11, only need the following:
+void *operator new  (size_t size) {
   return malloc(size);
 }
 
@@ -26,10 +37,131 @@ void *operator new[](size_t size) {
   return malloc(size);
 }
 
-void operator delete(void * ptr) {
+void  operator delete  (void * ptr) {
   free(ptr);
 }
 
-void operator delete[](void * ptr) {
+void  operator delete[](void * ptr) {
   free(ptr);
 }
+
+void * operator new  (size_t size, void * ptr) noexcept {
+  (void)size;
+  return ptr;
+}
+
+void * operator new[](size_t size, void * ptr) noexcept {
+  (void)size;
+  return ptr;
+}
+
+// Since C++14, also need two more delete variants...
+// Note thnat this CAN break code that compiled amd worked in C++11.
+#if (__cpp_sized_deallocation >= 201309L)
+  void  operator delete  (void* ptr, size_t size) noexcept {
+    (void) size;
+    free(ptr);
+  }
+  void  operator delete[](void* ptr, size_t size) noexcept {
+    (void) size;
+    free(ptr);
+  }
+#endif
+
+// Since C++17, there's four more each for new / delete, to support allocation
+// of objects with alignment greater than __STDCPP_DEFAULT_NEW_ALIGNMENT__.
+// Almost the entirety of AVR is using C++ 11. Any code that works elsewhere, but NOT on these cores and give these error messages either
+// is trying to get alignment that it doesn't actually need, or is - by sheet luck - ending up with the addresses which worked. E
+#if (__cpp_aligned_new >= 201606L)
+
+  void badAlloc(const char*) __attribute__((error("")));
+
+  void* operator new  (size_t size, std::align_val_t al) {
+    badAlloc("Alignment aware new/delete operators, a C++ 17 feaure, are not supported by this core or any other AVR cores at this point in time");
+    (void) al;
+    return malloc(size);
+  }
+
+/*
+void* operator new  (size_t size, std::align_val_t al) {
+
+  if (al <= alignof(std::max_align_t)) {
+      return malloc(size);
+  }
+
+  // how many extra bytes required to ensure can find an aligned pointer?
+  size_t toAllocate = size + (al - alignof(std::max_align_t)) - 1;
+  toAllocate += sizeof(uintptr_t);  // to store the original pointer
+
+  uintptr_t allocated = malloc(toAllocate);
+
+  // ensure to save space for the back-pointer.
+  uintptr_t alignedPtr = allocated + sizeof(uintptr_t);
+
+  size_t mask = al  1; // alignment mask ... the bits that must be zero
+  uintptr_t alignedPtr =
+      (allocated & mask) == 0 ?
+      allocated :
+      (allocated & (~mask)) + al;
+
+  // save the original pointer just before the pointer value returned to caller
+  static_assert(alignof(uintptr_t) <= alignof(std::max_align_t), "" );
+  uintptr_t* storeOriginalPointerAt = (uintptr_t)(alignedPtr - sizeof(uintptr_t));
+  *storeOriginalPointerAt = allocated;
+
+  return alignedPtr;
+}
+*/
+
+  void* operator new[](size_t size, std::align_val_t al) {
+    badAlloc("Alignment aware new/delete operators, a C++ 17 feaure, are not supported by this core or any other AVR cores at this point in time");
+    (void) al;
+    return malloc(size);
+  }
+  void  operator delete  (void* ptr, std::align_val_t al) noexcept {
+    badAlloc("Alignment aware new/delete operators, a C++ 17 feaure, are not supported by this core or any other AVR cores at this point in time");
+    (void) al;
+    free(ptr);
+  }
+  /*
+  void operator delete (void* ptr, std::align_val_t al) noexcept {
+
+    if (al <= STDCPP_DEFAULT_NEW_ALIGNMENT) {
+        free(ptr);
+        return;
+    }
+
+    // Get the original pointer value from just prior to the provided pointer.
+    uintptr_t aligned = ptr;
+    uintptr_t tmp = aligned - sizeof(uintptr_t); // go back 2 bytes
+    uintptr_t original = *((uintptr_t *)tmp); // extract the original pointer
+
+    // sanity check the original value... for iot, infinite loop recommended
+    // as it's better than memory corruption!rhy
+    size_t overhead = (al - STDCPP_DEFAULT_NEW_ALIGNMENT) + sizeof(uintptr_t);
+    assert(original < aligned);
+    assert(original + overhead <= aligned);
+
+    // and finally free the original memory
+    free(original);
+  }
+  */
+  void  operator delete[](void* ptr, std::align_val_t al) noexcept {
+    badAlloc("Alignment aware new/delete operators, a C++ 17 feaure, are not supported by this core or any other AVR cores at this point in time");
+    (void) al;
+    free(ptr);
+  }
+  void  operator delete  (void* ptr, size_t size, std::align_val_t al) noexcept{
+    badAlloc("Alignment aware new/delete operators, a C++ 17 feaure, are not supported by this core or any other AVR cores at this point in time");
+    (void) al;
+    (void) size;
+    free(ptr);
+  }
+  void  operator delete[](void* ptr, size_t size, std::align_val_t al) noexcept {
+    badAlloc("Overaligned allocation/deallocation is a C++ 17 feature, and is not supported by this or other AVR cores at this point in time");
+    (void) al;
+    (void) size;
+    free(ptr);
+  }
+
+#endif
--- avr/cores/tiny/new.h
+++ avr/cores/tiny/new.h
@@ -1,29 +1,60 @@
+/* New version (c) Spence Konde 2021
+ * This file is part of megaTinyCore, DxCore, and ATTinyCore.
+ * This is free software (released under the LGPL 2.1). For more
+ * information, see the License.md file included with this core.
+ */
+
 /*
-  Copyright (c) 2014 Arduino.  All right reserved.
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-  See the GNU Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+  C++11 needed some new new and delete operators.
+  Since C++14, also needed two more delete variants taking a size argument.
+    Said size argument is ignored (cast to void), but on some architectures
+    it is an important way to give hints to the memory allocator for greater
+    efficiency.
+  Since C++17, there's four more each for new / delete, to support allocation
+    of objects with alignment greater than __STDCPP_DEFAULT_NEW_ALIGNMENT__.
+    This has two major differences from the C++14 sized deallocaton.
+    1. It cannot be simply ignored - they presumably wanted alignment for
+    a reason; this implementation is not impossible, but nor is it trivial.
+    2. If code used on C++14 or earlier is asking for the alignment aware
+    versions of these operators, either it doesn't actually need it and
+    shouldn't be, or it was sheer happenstance that whatver address it
+    was getting worked within it's assumoptions.
+  Unless and until official cores or other cores in widespread use add support
+    for the alignment aware operators on AVRs, we have no plans to. While we
+    do not hesitate to offer users new features to take advantage of hardware
+    capabilities not present on older devices, this does not extend to new
+    compiler features which require modifications to code to take advantage
+    of. We have candidates for overaligned new and delete though, for when/if
+    they are required.
+    -Spence, 9/25/2021
+ */
 
 #ifndef NEW_H
-#define NEW_H
+  #define NEW_H
 
-#include <stdlib.h>
+  #include <stdlib.h>
+                                                /* C++11 and later                */
+  void* operator new   (size_t size             /* return malloc(size);           */);
+  void* operator new[] (size_t size             /* return malloc(size);           */);
+  void* operator new   (size_t size, void * ptr /* (void size); return ptr;       */) noexcept;
+  void* operator new[] (size_t size, void * ptr /* (void size); return ptr;       */) noexcept;
+  void operator delete   (void* ptr             /* free(ptr);                     */) noexcept;
+  void operator delete[] (void* ptr             /* free(ptr);                     */) noexcept;
 
-void * operator new(size_t size);
-void * operator new[](size_t size);
-void operator delete(void * ptr);
-void operator delete[](void * ptr);
+  #if (__cpp_sized_deallocation >= 201309L)       /* C++14 and later only         */
+    void operator delete   (void* ptr, size_t size /* (void)size; return ptr;     */) noexcept;
+    void operator delete[] (void* ptr, size_t size /* (void)size; return ptr;     */) noexcept;
+  #endif
 
+  #if (__cpp_aligned_new >= 201606L) /* C++17 and later only                      */
+    /* These are unsupported and just stubs that generate a more helpful error    */
+    /* If it turns out that we do need them, we have a candidate for these        */
+    void* operator new     (size_t size, std::align_val_t al                        );
+    void  operator delete  (void* ptr,/* (void)al; return ptr; */std::align_val_t al) noexcept;
+    /* Not actually supported, these stubs just generate a more helpful error   */
+    void* operator new[]   (size_t size, std::align_val_t al                        );
+    void  operator delete[](void* ptr,/* (void)al; return ptr; */std::align_val_t al) noexcept;
+    void  operator delete  (void* ptr, size_t size,/* above +  */std::align_val_t al) noexcept;
+    void  operator delete[](void* ptr, size_t size,/* (void)al;*/std::align_val_t al) noexcept;
+  #endif
 #endif
