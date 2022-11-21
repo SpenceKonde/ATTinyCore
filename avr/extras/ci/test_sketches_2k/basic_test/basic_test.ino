8085baccb9218c32a4239222ae07c48c0adad572
Stephan Martin <designer2k2@gmail.com>
2022-11-21 23:08:39 +0100

    go for LED_BUILTIN

--- avr/extras/ci/test_sketches_2k/basic_test/basic_test.ino
+++ avr/extras/ci/test_sketches_2k/basic_test/basic_test.ino
@@ -1,5 +1,3 @@
-#define GPIO0 0
-
 void setup() {
   // put your setup code here, to run once:
   digitalRead(3);
@@ -7,9 +5,9 @@ void setup() {
   pinMode(3,INPUT);
   pinMode(3,INPUT_PULLUP);
   pinMode(3,OUTPUT);
-  digitalWrite(GPIO0,HIGH);
-  digitalRead(GPIO0);
-  pinMode(GPIO0,OUTPUT);
+  digitalWrite(LED_BUILTIN ,HIGH);
+  digitalRead(LED_BUILTIN);
+  pinMode(LED_BUILTIN ,OUTPUT);
 }
 
 void loop() {
